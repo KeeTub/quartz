de036596434e6ccc45fe95a01faac51a2a903993
KeeTub <152419173+KeeTub@users.noreply.github.com>
2026-02-26 17:15:27 -0700

    Update sw.js

--- quartz/static/sw.js
+++ quartz/static/sw.js
@@ -1,20 +1,8 @@
-const CACHE_NAME = 'pxkor-bible-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  // 추가적으로 오프라인에서 꼭 보여야 할 CSS나 JS 파일 경로를 넣으세요.
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  // PWA 설치 조건을 충족하기 위한 빈 fetch 이벤트
+  e.respondWith(fetch(e.request));
 });
