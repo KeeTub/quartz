27aec31aaf2859810eecda2c633944389d1645a8
KeeTub <152419173+KeeTub@users.noreply.github.com>
2026-02-26 17:34:17 -0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,19 +1,10 @@
-// 서비스 워커 설치
-self.addEventListener('install', (event) => {
-  console.log('pxkorBible Service Worker 설치됨');
+// static/sw.js
+self.addEventListener('install', (d) => {
+  console.log('Service Worker 설치 완료');
   self.skipWaiting();
 });
 
-// 서비스 워커 활성화
-self.addEventListener('activate', (event) => {
-  console.log('pxkorBible Service Worker 활성화됨');
-});
-
-// 설치 아이콘이 뜨기 위한 필수 조건 (Fetch 이벤트 처리)
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+  // 설치 조건을 충족하기 위한 필수 fetch 이벤트 처리
+  event.respondWith(fetch(event.request));
 });
