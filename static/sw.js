5764b53cd3b69776b27763a04cdc4c416eeb206b
KeeTub <152419173+KeeTub@users.noreply.github.com>
2026-02-26 17:53:54 -0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,8 @@
-// static/sw.js
-self.addEventListener('install', (d) => {
-  console.log('Service Worker 설치 완료');
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-  // 설치 조건을 충족하기 위한 필수 fetch 이벤트 처리
+  // PWA 설치 요건을 충족하기 위한 필수 fetch 이벤트
   event.respondWith(fetch(event.request));
 });
