70d93ba640273621679c03702bcee108e0da596b
KeeTub <152419173+KeeTub@users.noreply.github.com>
2026-02-26 17:17:18 -0700

    Update sw.js

--- quartz/static/sw.js
+++ quartz/static/sw.js
@@ -1,8 +1,19 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+// 서비스 워커 설치
+self.addEventListener('install', (event) => {
+  console.log('pxkorBible Service Worker 설치됨');
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  // PWA 설치 조건을 충족하기 위한 빈 fetch 이벤트
-  e.respondWith(fetch(e.request));
+// 서비스 워커 활성화
+self.addEventListener('activate', (event) => {
+  console.log('pxkorBible Service Worker 활성화됨');
+});
+
+// 설치 아이콘이 뜨기 위한 필수 조건 (Fetch 이벤트 처리)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
