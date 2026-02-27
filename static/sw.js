2e813dbb35f7752a04f21e1c30a0e9bfdb9bfb2e
KeeTub <152419173+KeeTub@users.noreply.github.com>
2026-02-26 17:59:18 -0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,19 @@
+// static/sw.js
+const CACHE_NAME = 'bible-cache-v1';
+
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// 설치 메뉴가 나타나기 위해 반드시 필요한 fetch 이벤트
 self.addEventListener('fetch', (event) => {
-  // PWA 설치 요건을 충족하기 위한 필수 fetch 이벤트
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
